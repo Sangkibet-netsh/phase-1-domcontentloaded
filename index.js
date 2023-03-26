fd7347ec93152a8e88cac40df99f395b70d186c9
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-26 14:05:29 +0300

    Done With Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // const elem = document.getElementById(text)//displays the initial text in the index.html file
+    const elem = document.querySelector("#text")//replaces the text in the index.html
+    elem.textContent = "This is really cool!"
+    console.log(elem);
+  });
+  
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
